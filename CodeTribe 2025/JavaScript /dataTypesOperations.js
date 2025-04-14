c1ff80ffd80b3da2667083d81249783ec334313c
wavelink21 <131858507+wavelink21@users.noreply.github.com>
2025-04-14 04:41:30 +0000

    Initial commit with dataTypesOperations.js

---
+++ CodeTribe 2025/JavaScript /dataTypesOperations.js
@@ -0,0 +1,30 @@
+//Exercise 1
+
+var sum;
+var difference;
+var product;
+var quotient;
+var modulus;
+var exponentiation;
+
+let Num1 = 23; 
+let Num2 = 14.12;
+let Num3 = 5.67;
+let Num4 = 8;
+
+sum = Num1 + Num2 + Num3 + Num4;
+difference = Num1 - Num2 - Num3 - Num4;     
+product = Num1 * Num2 * Num3 * Num4;
+quotient = Num1 / Num2 / Num3 / Num4;
+modulus = Num1 % Num2;
+exponentiation = Num1 ** Num2;
+
+console.log("Sum: " + sum);
+console.log("Difference: " + difference);
+console.log("Product: " + product);
+console.log("Quotient: " + quotient);
+console.log("Modulus: " + modulus);
+console.log("Exponentiation: " + exponentiation);      
+//Exercise 2
+
+
---
+++ CodeTribe 2025/JavaScript /variable.js
@@ -0,0 +1,44 @@
+//Ecersise 1
+
+var fullName;
+var name = "Kabelo";
+var surname = "Motaung";
+
+fullName = name + " " + surname;
+console.log(fullName);
+
+var age = 25;
+console.log(age);
+
+var isStudent;
+isStudent = true;
+console.log(isStudent);
+
+//Ecersise 2
+
+favColor = "blue";
+console.log(favColor);
+
+favColor = "bage";
+console.log(favColor);
+
+//Ecersise 3
+var num1 = 5;
+var num2 = 10;
+var num3 = 15;
+
+var sum;
+var difference;
+var product;
+var quotient;
+
+sum = num1 + num2 + num3;
+difference = num1 - num2 - num3;
+product = num1 * num2 * num3;
+quotient = num1 / num2 / num3;
+
+console.log("Sum: " + sum);
+console.log("Difference: " + difference);       
+console.log("Product: " + product);
+console.log("Quotient: " + quotient);
+
