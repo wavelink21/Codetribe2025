d599ee3c3b1f893cf199c0b9fb880730019d810c
wavelink21 <131858507+wavelink21@users.noreply.github.com>
2025-04-16 04:51:39 +0000

    Add memory game files

--- CodeTribe 2025/JavaScript /dataTypesOperations.js
+++ CodeTribe 2025/JavaScript /dataTypesOperations.js
@@ -24,7 +24,31 @@ console.log("Difference: " + difference);
 console.log("Product: " + product);
 console.log("Quotient: " + quotient);
 console.log("Modulus: " + modulus);
-console.log("Exponentiation: " + exponentiation);      
+console.log("Exponentiation: " + exponentiation);  
+
 //Exercise 2
+
+var sumBool;
+sumBool = true + false;
+var x= 8;
+var y= 12;
+sumBool = x + y;
+console.log("Sum of boolean values: " + sumBool);
+
+let isGreater = x > y; 
+let isLess = x < y;  
+let isEqual = y == x; 
+let isNotEqual = x != y; 
+let isGreaterOrEqual = y>= x; 
+let isLessOrEqual = x <= y;
+
+console.log("Is x greater than y? " + isGreater);
+console.log("Is x less than y? " + isLess);
+console.log("Is y equal to y? " + isEqual);
+console.log("Is x not equal to y? " + isNotEqual);
+console.log("Is y greater than or equal to x? " + isGreaterOrEqual);
+console.log("Is x less than or equal to y? " + isLessOrEqual);
+
+
 
 
